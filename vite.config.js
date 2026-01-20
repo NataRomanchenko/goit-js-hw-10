49aec4486280bffa7f3cb9c54759922d98ddca52
Nataliia_Romanchenko <rnataliia36gmail.com>
2026-01-20 23:07:35 +0200

    HW-10: countdown timer & promise generator

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite';
+
+    import { defineConfig } from 'vite';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
@@ -6,10 +7,25 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-js-hw-10/',
+
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+
     root: 'src',
+
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/*.html']),
+    ],
+
+    css: {
+      postcss: {
+        plugins: [SortCss()],
+      },
+    },
+
     build: {
       sourcemap: true,
       rollupOptions: {
@@ -37,12 +53,5 @@ export default defineConfig(({ command }) => {
       outDir: '../dist',
       emptyOutDir: true,
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
   };
 });
